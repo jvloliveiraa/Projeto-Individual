4c7e147559feb8375ef4c077f99711805b793f69
jvloliveiraa <joao.oliveira@sptech.school>
2022-05-06 10:06:04 -0300

    Criando div de cadastro

---
+++ Projeto Individual/JS/Funcionalidades.js
@@ -0,0 +1,13 @@
+/* Tela inicial */
+function go_cadastro(){
+    div_cadastro.style.display = 'block';
+}
+
+
+
+
+
+/* Tela cadastro */
+function go_indexByCadastro(){
+    div_cadastro.style.display = 'none';
+}
