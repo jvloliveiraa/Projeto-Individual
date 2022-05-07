900ef51a12297281c69fad368e363db8d513190f
jvloliveiraa <joao.oliveira@sptech.school>
2022-05-07 19:09:23 -0300

    Começando telas novas

--- Projeto Individual/JS/Funcionalidades.js
+++ Projeto Individual/JS/Funcionalidades.js
@@ -1,4 +1,4 @@
-/* Tela inicial */
+/* Header inicial */
 function go_cadastro(){
     div_cadastro.style.display = 'block';
 }
@@ -7,8 +7,11 @@ function go_login(){
     div_login.style.display = 'block';
 }
 
+/* Header principal */
 
-
+function go_index(){
+    window.location.href = "Index.html";
+}
 
 /* Tela cadastro */
 function go_indexByCadastro(){
