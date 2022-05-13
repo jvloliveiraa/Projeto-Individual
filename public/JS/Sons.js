a704c7d35abefc3e2763c30b9705c3438c0dca5b
jvloliveiraa <joao.oliveira@sptech.school>
2022-05-13 12:45:46 -0300

    Player quase finalizado

--- public/JS/Sons.js
+++ public/JS/Sons.js
@@ -1,33 +1,92 @@
-var contadorClick = 0;
 const musicas = [
-    'audios/02 Beginning of the Journey.mp3', //[0]
-    'audios/02 Overworld.mp3', //[1]
-    'audios/08 Hyrule Field Main Theme.mp3', //[2]
-    'audios/08 The Legend of Zelda.mp3', //[3]
-    'audios/1-04-Ocarina Melodies Suite 2018.mp3', //s[4]
-    'audios/25 The Goddess Appears.mp3', //[5]
-    "audios/27 - Ocarina 'Epona's Song'.mp3", //[6]
-    "audios/43 - Ocarina 'Song of Time'.mp3", //[7]
+    'Sons/Som0-Saudade.mp3', //[0]
+    'Sons/Som1-Collapse.mp3', //[1]
+    'Sons/Som2-RPDHall.mp3', //[2]
+    'Sons/Som3-BlackImpact.mp3', //[3]
+    'Sons/Som4-Consequence.mp3', //[4]
+    'Sons/Som5-Adventure.mp3', //[5]
+    "Sons/Som6-Reliving.mp3", //[6]
+    "Sons/Som7-Mournful.mp3", //[7]
+
+    //autoplay = inicia sozinho; loop = faz repetir; played = indica que o audio foi todo reproduzido
 ];
 
-function playOuPause() {
-    contadorClick++
+function tocar0(){
+    audio.src = `${musicas[0]}`;
+    tocando_agora.innerHTML = `Tocando agora: ${musicas[0]}`;
+    audio.play();
+}
 
-    if (contadorClick % 2 == 0) {
-        audioAleatorio.pause();
+function tocar1(){
+    audio.src = `${musicas[1]}`;
+    tocando_agora.innerHTML = `Tocando agora: ${musicas[1]}`;
+    audio.play();
+}
 
-        msgPrimaria.style.display = `block`;
-        msgDisplay.style.display = `none`;
-
-    } else {
-        let trilhaSorteada = parseInt(Math.random() * 9)
-
-        audioAleatorio.src = `${musicas[trilhaSorteada]}`;
-        audioAleatorio.play();
-        audioAleatorio.volume = 0.15;
-        audioAleatorio.loop = `true`;
-
-        msgDisplay.style.display = `block`;
-        msgPrimaria.style.display = `none`;
+function tocar2(){
+    var contador = 0;
+    while(contador <= 7){
+        var verificar_id = 'musica' + contador;
+        if(verificar_id == 'musica2'){
+            tocando_agora.innerHTML = `Tocando agora: ${musicas[contador]}`;
+            audio.src = `${musicas[contador]}`;
+            audio.play();
+        }
+        contador++;
     }
+}
+function tocar3(){
+    var contador = 0;
+    for(contador; contador <= 7; contador++){
+        var verificar_id = 'musica' + contador;
+        if(verificar_id == 'musica3'){
+            tocando_agora.innerHTML = `Tocando agora: ${musicas[contador]}`;
+            audio.src = `${musicas[contador]}`;
+            audio.play();
+        }
+    }
+}
+
+function tocar4(){
+    audio.src = `${musicas[4]}`;
+    tocando_agora.innerHTML = `Tocando agora: ${musicas[4]}`;
+    audio.play();
+}
+
+function tocar5(){
+    audio.src = `${musicas[5]}`;
+    tocando_agora.innerHTML = `Tocando agora: ${musicas[5]}`;
+    audio.play();
+}
+
+function tocar6(){
+    audio.src = `${musicas[6]}`;
+    tocando_agora.innerHTML = `Tocando agora: ${musicas[6]}`;
+    audio.play();
+}
+
+function tocar7(){
+    audio.src = `${musicas[7]}`;
+    tocando_agora.innerHTML = `Tocando agora: ${musicas[7]}`;
+    audio.play();
+}
+
+function play(){
+    audio.play();
+}
+
+function pausar(){
+    audio.pause();
+}
+
+function parar(){
+    audio.pause();
+    audio.currentTime = 0;
+}
+
+function aleatorio(){
+    var trilhaSorteada = parseInt(Math.random() * 8)
+
+    audio.src = `${musicas[trilhaSorteada]}`;
+    audio.play();
 }
